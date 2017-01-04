7957577e124742e26a9c6062c94e3053fb77c4cf
PixelZerg <abhinavbhandari4@gmail.com>
2017-01-04 15:15:57 +0000

    updts

---
+++ SlidingPuzzleSolver/Board.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlidingPuzzleSolver
+{
+    public enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+
+    public class Board
+    {
+        public int[,] board { get; private set;}
+
+        public Board() { }
+
+        /// <param name="order">Essentially "width"</param>
+        public Board(int order)
+        {
+            this.Initialise(order);
+        }
+
+        /// <param name="order">Essentially "width"</param>
+        public void Initialise(int order)
+        {
+            board = new int[order, order];
+        }
+    }
+}
---
+++ SlidingPuzzleSolver/Point.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlidingPuzzleSolver
+{
+    public class Point
+    {
+        public int X = -1;
+        public int Y = -1;
+
+        public Point()
+        {
+        }
+
+        public Point(int X, int Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+    }
+}
